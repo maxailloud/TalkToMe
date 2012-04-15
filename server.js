47d71df2585ad29f770eca23ac468384d3b2413e
Maxime AILLOUD <maxime.ailloud@gmail.com>
2012-04-15 22:03:43 +0200

    Debut de gestion des salons

--- server.js
+++ server.js
@@ -20,6 +20,9 @@ app.configure(function() {
     // Allow parsing cookies from request headers
     this.use(express.cookieParser());
 
+    // Allow parsing form data
+    this.use(express.bodyParser());
+
     // Session management
     // Internal session data storage engine, this is the default engine embedded with connect.
     // Much more can be found as external modules (Redis, Mongo, Mysql, file...).
@@ -49,11 +52,6 @@ io.sockets.on('connection', function(socket)
     socket.on('authenticate', function(username)
     {
         socket.emit('authenticated', username);
-    });
-
-    socket.on('rooms', function(username)
-    {
-        socket.emit('rooms', io.sockets.manager.rooms);
     });
 });
 
@@ -98,16 +96,113 @@ io.sockets.authorization(function(handshakeData, callback)
     }
 });
 
+// Middleware for limited access
+function requireLogin (req, res, next)
+{
+    if(req.session.username)
+    {
+        // User is authenticated, let him go
+        next();
+    }
+    else
+    {
+        // Otherwise, we redirect him to login form
+        res.redirect("/login");
+    }
+}
+
 // Routes
-app.get('/', function (req, res, next) {
-    res.render('index');
+app.get('/', [requireLogin], function (req, res, next)
+{
+    var rooms = io.sockets.manager.rooms;
+
+    // Delete default room
+    delete rooms[''];
+
+    var roomList = '';
+    var nbRoom = 0;
+
+    for(var i in rooms)
+    {
+        roomList += '<li><a href="/room/"' + i + ' title="Go to ' + i + ' room">' + i + '</a>(' + rooms[i].length + ')</li>';
+        nbRoom++;
+    }
+
+    if(0 == nbRoom)
+    {
+        roomList = '<li>Aucun salon</li>';
+    }
+
+    res.render('index', { 'username': req.session.username, 'rooms': roomList });
 });
 
-app.get('/rooms', function (req, res, next) {
-    res.render('rooms');
+app.get("/login", function (req, res)
+{
+    // Show form, default value = current username
+    res.render("login", { "username": req.session.username, "error": null });
+});
+app.post("/login", function (req, res)
+{
+    var options = { "username": req.body.username, "error": null };
+
+    if(!req.body.username)
+    {
+        options.error = "User name is required";
+        res.render("login", options);
+    }
+    else if(req.body.username == req.session.username)
+    {
+        // User has not changed username, accept it as-is
+        res.redirect("/");
+    }
+    else if(!req.body.username.match(/^[a-zA-Z0-9\-_]{3,}$/))
+    {
+        options.error = "User name must have at least 3 alphanumeric characters";
+        res.render("login", options);
+    }
+    else
+    {
+        // Validate if username is free
+        req.sessionStore.all(function (err, sessions)
+        {
+            if(!err)
+            {
+                var found = false;
+                for(var i = 0; i < sessions.length; i++)
+                {
+                    var session = JSON.parse(sessions[i]); // Si les sessions sont stockées en JSON
+                    if(session.username == req.body.username)
+                    {
+                        err = "User name already used by someone else";
+                        found = true;
+                        break;
+                    }
+                }
+            }
+            if(err)
+            {
+                options.error = ""+err;
+                res.render("login", options);
+            }
+            else
+            {
+                req.session.username = req.body.username;
+                res.redirect("/");
+            }
+        });
+    }
+});
+
+app.get('/room/:room', function (req, res, next)
+{
+    console.log(req.params.room);
+    var rooms = io.sockets.manager.rooms;
+    console.log(rooms);
+    res.render('room', { "username": req.session.username });
 });
 
 // Start server
-app.listen(port, function(){
+app.listen(port, function()
+{
     console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
